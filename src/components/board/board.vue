c9a6b8c65828d0b1043c94b3dd8b5ff1e366c5aa
jubin thomas <athom130@adient.com>
2024-01-23 18:44:24 +0530

    initial commit

---
+++ src/components/board/board.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h4>Current Player: {{ playervar }}</h4>
+
+    <button v-on:click="newGame()">Start new Game</button>
+
+    <h5>Player {{ winner }} won the game!</h5>
+    <div class="tictactoe">
+        <Square v-for="(value, index) in squares" :value="value" :key="index" @click="makeMove(index)" />
+    </div>
+  </div>
+</template>
+
+<script src="./board.js">
+</script>
+
+<style src="./board.css" scoped></style>
---
+++ src/components/square/square.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="square">
+    <button :data-player="value" @click="$emit('click')">{{ value }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Square",
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.square {
+  flex: 1;
+  flex-basis: 30%;
+  border: 1px solid gray;
+}
+
+button {
+  width: 100%;
+  height: 60px;
+}
+
+button[data-player="X"] {
+  color: darkgreen;
+  background-color: aquamarine;
+}
+
+button[data-player="O"] {
+  color: lightcoral;
+  background-color: blanchedalmond;
+}
+</style>
